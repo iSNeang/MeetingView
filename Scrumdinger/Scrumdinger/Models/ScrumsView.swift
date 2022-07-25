8f8fc0c84052f8b57b5d2d6a15ce6be21d0175f8
Neang <neangsd.maria@gmail.com>
2022-07-25 23:50:37 +0800

    Display a List of Daily Scrums

---
+++ Scrumdinger/Scrumdinger/Models/ScrumsView.swift
@@ -0,0 +1,30 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Kon Khmer on 7/25/22.
+//
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                    CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+        
+    }
+}
